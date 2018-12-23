2cb0db0709ad17369c17fc1ce083503bba31bae0
Leland Sindt <leland@sway.org>
2018-12-23 13:52:06 -0600

    initial communication with/dependency on lumen

--- query.py
+++ query.py
@@ -6,18 +6,30 @@ import time
 import datetime
 import pprint
 import json
+import requests
 from geopy.distance import geodesic
+try:
+  home = open('/var/run/secrets/home', 'r').read().strip()
+  username = open('/var/run/secrets/email', 'r').read().strip()
+  password = open('/var/run/secrets/passworda', 'r').read().strip()
+except: #Todo come up with better defaults.
+  home = None
+  username = None
+  password = None
 
-home = open('/var/run/secrets/home', 'r').read().strip()
+try:
+  minimumRange = open('/var/run/secrets/minrange', 'r').read().strip()
+except:
+  minimumRange = 100
+
 maxDistanceFt = 100
-username = open('/var/run/secrets/email', 'r').read().strip()
-password = open('/var/run/secrets/password', 'r').read().strip() 
 getStateIntervalDefault = 3600
-getStateInterval = getStateIntervalDefault 
+getStateInterval = getStateIntervalDefault
 getStateIntervalActive = 5 
 getStateIntervalOnline = 900 
 getStateIntervalOnline = 1200 
-getStateIntervalOnline = getStateIntervalDefault 
+getStateIntervalOnline = getStateIntervalDefault
+loginfailloop = 90
 debugEnabled = True
 
 def signal_handler(sig, frame):
@@ -37,13 +49,18 @@ def initializeState():
   state['data_state']['isGood'] = False
   return state
 
+def lumenPUT(data):
+  lumenPUT = requests.put('http://lumen:9000/lumen', data=data)
+  pdebug('response code: ' + str(lumenPUT.status_code) + ', response json: ' + lumenPUT.text )
+
+
 def getVehicle():
   pdebug('start getVehicle')
   try:
     connection = teslajson.Connection(username, password, tesla_client='{"v1": {"id": "e4a9949fcfa04068f59abb5a658f2bac0a3428e4652315490b659d5ab3f35a9e", "secret": "c75f14bbadc8bee3a7594412c31416f8300256d7668ea7e6e7f06727bfb9d220", "baseurl": "https://owner-api.teslamotors.com", "api": "/api/1/"}}')
     return connection.vehicles[0]
   except: 
-    return {'state' : 'failed'} 
+    return {'state' : 'failed'}
 
 def getState(vehicle):
   pdebug('start getState')
@@ -75,9 +92,9 @@ def getState(vehicle):
   state['data_state']['isGood'] = True 
   return state
 
-state = initializeState() 
+state = initializeState()
 
-# soft check every 15 minutes if car is online, then get state.  
+# Todo: soft check every 15 minutes if car is online, then get state.
 
 while True:
   if int(time.time()) - int(state['data_state']['timestamp'])  > getStateInterval:
@@ -86,16 +103,30 @@ while True:
       state = getState(vehicle)
       if state['data_state']['isGood'] == True:
         #pprint.pprint(state, indent=2)
-        pdebug('shift_state: ' + str(state['drive_state']['shift_state']) + ' speed: ' + str(state['drive_state']['speed']) + ' distance from home: ' + str(state['vehicle_state']['distanceFromHome']))
+        pdebug('shift_state: ' + str(state['drive_state']['shift_state']) + ', speed: ' + str(state['drive_state']['speed']) + ', distance from home: ' + str(state['vehicle_state']['distanceFromHome']) + ', range: ' + str(state['charge_state']['battery_range']) )
         if state['vehicle_state']['isHome'] == True and state['charge_state']['charging_state'] != 'Disconnected':
           pdebug('Car is home and plugged in!')
-        if state['vehicle_state']['isHome'] == True and state['charge_state']['charging_state'] == 'Disconnected':
+          lumenPUT('{"animation":"fill","g":255}')
+        elif state['vehicle_state']['isHome'] == True and state['charge_state']['charging_state'] == 'Disconnected' and state['charge_state']['battery_range'] < minimumRange:
+          pdebug('car is home, not plugged in and below ' + minimumRange + ' miles range')
+          lumenPUT('{"animation":"fill","r":255}')
+        elif state['vehicle_state']['isHome'] == True and state['charge_state']['charging_state'] == 'Disconnected':
           pdebug('Warning car is home but not plugged in!')
-        if state['vehicle_state']['isHome'] == False:
+          lumenPUT('{"animation":"fill","r":255,"g":255}')
+        elif state['vehicle_state']['isHome'] == False:
           pdebug('Car is not at home')
-        if state['drive_state']['shift_state'] != None:
-          getStateInterval = getStateIntervalActive 
+          lumenPUT('{"animation":"rainbow"')
+        elif state['drive_state']['shift_state'] != None:
+          getStateInterval = getStateIntervalActive
+          lumenPUT('{"animation":"rainbow"')
         else:
           getStateInterval = getStateIntervalOnline 
+    else:
+      pdebug('login failed, sleeping for a while')
+      for i in range(loginfailloop):
+        lumenPUT('{"animation":"fill","r":255}')
+        time.sleep(1)
+        lumenPUT('{"animation":"fill"}')
+        time.sleep(1)
     pdebug('next check: ' + str(datetime.datetime.now() + datetime.timedelta(seconds=getStateInterval)) + ' (' + str(getStateInterval) + ')')
   time.sleep(.5)
